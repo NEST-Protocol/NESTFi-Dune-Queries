bef4071778d830a8bd92821d570c47f4278c433a
tunogya <tunogya@qq.com>
2023-02-01 18:36:59 +0800

    update

--- queries/weekly-active-users-of-nestfi.sql
+++ queries/weekly-active-users-of-nestfi.sql
@@ -7,11 +7,11 @@ FROM (SELECT evt_block_time,
              "from" as address
       FROM nestfi_bnb.NEST_evt_Transfer
 --       0x65e7506244cddefc56cd43dc711470f8b0c43bee is the address of NestVault
-      WHERE "to" = '0x65e7506244cddefc56cd43dc711470f8b0c43bee'
+      WHERE "to" = 0x65e7506244cddefc56cd43dc711470f8b0c43bee
       UNION
       SELECT evt_block_time,
              "to" as address
       FROM nestfi_bnb.NEST_evt_Transfer
-      WHERE "from" = '0x65e7506244cddefc56cd43dc711470f8b0c43bee') as nestfi_data
+      WHERE "from" = 0x65e7506244cddefc56cd43dc711470f8b0c43bee) as nestfi_data
 GROUP BY 1
 ORDER BY 1

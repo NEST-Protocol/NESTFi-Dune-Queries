3c02be866651539f141794bd00f9e8125e623a67
tunogya <tunogya@qq.com>
2023-02-02 11:36:17 +0800

    update

--- queries/weekly-nest-burned-of-bsc.sql
+++ queries/weekly-nest-burned-of-bsc.sql
@@ -1,19 +1,21 @@
 -- Engine: Dune Engine v2 (Dune SQL)
 -- Project: nestfi
 -- Network: bnb
-WITH in_amount as (SELECT DATE_TRUNC('week', evt_block_time) AS block_date,
-                          cast(SUM(cast(value as uint256)) / cast(1e18 as uint256) as bigint)       as amount
+WITH in_amount as (SELECT DATE_TRUNC('week', evt_block_time)                                  AS block_date,
+                          cast(SUM(cast(value as uint256)) / cast(1e18 as uint256) as bigint) as amount
                    FROM nestfi_bnb.NEST_evt_Transfer
                    WHERE "to" = 0x65e7506244cddefc56cd43dc711470f8b0c43bee
+                     AND evt_block_time > date('2022-07-25')
                    GROUP BY 1
                    ORDER BY 1),
-     out_amount as (SELECT DATE_TRUNC('week', evt_block_time) AS block_date,
-                           cast(SUM(cast(value as uint256)) / cast(1e18 as uint256) as bigint)        as amount
+     out_amount as (SELECT DATE_TRUNC('week', evt_block_time)                                  AS block_date,
+                           cast(SUM(cast(value as uint256)) / cast(1e18 as uint256) as bigint) as amount
                     FROM nestfi_bnb.NEST_evt_Transfer
                     WHERE "from" = 0x65e7506244cddefc56cd43dc711470f8b0c43bee
+                      AND evt_block_time > date('2022-07-25')
                     GROUP BY 1
                     ORDER BY 1)
-SELECT in_amount.block_date as block_date,
+SELECT in_amount.block_date                 as block_date,
        in_amount.amount - out_amount.amount as burn_amount
 FROM in_amount
          FULL OUTER JOIN out_amount

10844337f0d0f475f201c944c060119877e69b57
tunogya <tunogya@qq.com>
2023-02-02 13:37:53 +0800

    update

--- queries/long-open-interest.sql
+++ queries/long-open-interest.sql
@@ -5,7 +5,7 @@ WITH all_buy2_long AS (SELECT lever,
                               cast(amount as bigint) as amount,
                               orientation,
                               tokenIndex,
-                              call_tx_hash as hash
+                              call_tx_hash           as hash
                        FROM nestfi_bnb.NestFutures2_call_buy2
                        WHERE orientation = TRUE),
      all_buy2_log AS (SELECT index,
@@ -17,15 +17,26 @@ WITH all_buy2_long AS (SELECT lever,
                   FROM nestfi_bnb.NestFutures2_call_sell2),
      all_buy2_long_data AS (SELECT all_buy2_long.lever,
                                    all_buy2_long.amount,
-                                   all_buy2_long.orientation,
-                                   all_buy2_long.tokenIndex,
                                    all_buy2_log.index
                             FROM all_buy2_long
                                      LEFT JOIN all_buy2_log
-                                               ON all_buy2_long.hash = all_buy2_log.hash)
-SELECT SUM(all_buy2_long_data.lever * all_buy2_long_data.amount) as total
-FROM all_buy2_long_data
-WHERE all_buy2_long_data.index NOT IN (SELECT index
-                                       FROM all_settle)
-  AND all_buy2_long_data.index NOT IN (SELECT index
-                                       FROM all_sell)
+                                               ON all_buy2_long.hash = all_buy2_log.hash),
+     add_data AS (SELECT cast(amount as bigint) as amount,
+                         index
+                  FROM nestfi_bnb.NestFutures2_call_add2),
+     add_full_data AS (SELECT add_data.amount * all_buy2_long_data.lever as total
+                       FROM add_data
+                                LEFT JOIN all_buy2_long_data
+                                          ON add_data.index = all_buy2_long_data.index),
+     add_full_data_sum AS (SELECT SUM(total) as total
+                           FROM add_full_data),
+     buy_full_data_sum AS (
+         SELECT SUM(all_buy2_long_data.lever * all_buy2_long_data.amount) as total
+         FROM all_buy2_long_data
+         WHERE all_buy2_long_data.index NOT IN (SELECT index
+                                                FROM all_settle)
+           AND all_buy2_long_data.index NOT IN (SELECT index
+                                                FROM all_sell)
+     )
+SELECT buy_full_data_sum.total + add_full_data_sum.total as total
+FROM buy_full_data_sum, add_full_data_sum
